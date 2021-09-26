a58314edfeef0de3bf6996884c4eabac0b806aea
piresthiago10 <thiagopires.as@gmail.com>
2021-09-26 00:52:01 -0300

    criado model para conversor_moedas

--- conversor_moedas/models.py
+++ conversor_moedas/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Conversao(models.Model):
+   MOEDA = (
+       ('USD', 'Dollar'),
+       ('BRL', 'Real'),
+       ('EUR', 'Euro'),
+       ('BTC', 'Bitcoin'),
+       ('ETH', 'Ethereum')
+   )
+   moeda_origem = models.CharField(max_length=3, choices=MOEDA, blank=False, default='USD')
+   moeda_final = models.CharField(max_length=3, choices=MOEDA, blank=False, default='BRL')
+   valor_conversao = models.FloatField()
+   valor_convetido = models.FloatField()
+   data_hora = models.DateField()
+
+   def __str__(self) -> str:
+       return self.moeda_origem + "-" + self.moeda_final
