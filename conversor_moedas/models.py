1d505a105aed15862dc1acc610fdbfe564a8aa0b
piresthiago10 <thiagopires.as@gmail.com>
2021-09-27 15:39:32 -0300

    feature - atributos de Conversao aprimorados

--- conversor_moedas/models.py
+++ conversor_moedas/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.expressions import F
 
 class Conversao(models.Model):
    MOEDA = (
@@ -10,9 +11,9 @@ class Conversao(models.Model):
    )
    moeda_origem = models.CharField(max_length=3, choices=MOEDA, blank=False, default='USD')
    moeda_final = models.CharField(max_length=3, choices=MOEDA, blank=False, default='BRL')
-   valor_conversao = models.FloatField()
-   valor_convertido = models.FloatField(null=True)
-   data_hora = models.DateField(auto_now=True)
+   valor_conversao = models.FloatField(blank=False)
+   valor_convertido = models.FloatField(blank=False)
+   data_hora = models.DateTimeField(auto_now=True)
 
    def __str__(self) -> str:
        return self.moeda_origem + "-" + self.moeda_final
