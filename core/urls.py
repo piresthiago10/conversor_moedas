cd046c9371077719ba48faa3a08afca3466d5cd3
piresthiago10 <thiagopires.as@gmail.com>
2021-09-26 23:52:39 -0300

    urls de conversor de moedas

--- core/urls.py
+++ core/urls.py
@@ -1,21 +1,15 @@
-"""core URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from conversor_moedas.api.viewsets import ConversaoViewSet, ListaConversoesViewSet, ListaConversoes
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('conversao', ConversaoViewSet, basename='Conversao')
+router.register('lista', ListaConversoesViewSet, basename='lista')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('from/<str:moeda_origem>/to/<str:moeda_final>/amount/<int:valor_conversao>', ListaConversoesViewSet, name="conversor"),
+    path('listagem', ListaConversoes.as_view(), name="listagem")
 ]
