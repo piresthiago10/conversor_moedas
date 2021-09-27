233140f9b6107a5ff2effb58684835dc5b4e36c6
piresthiago10 <thiagopires.as@gmail.com>
2021-09-27 15:46:24 -0300

    removidos rota e path viewset removida

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from conversor_moedas.api.viewsets import ConversaoViewSet, ListaConversoesViewSet, ListaConversoes
+from conversor_moedas.api.viewsets import ConversaoViewSet, ListaConversoes
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('conversao', ConversaoViewSet, basename='Conversao')
-router.register('lista', ListaConversoesViewSet, basename='lista')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('from/<str:moeda_origem>/to/<str:moeda_final>/amount/<int:valor_conversao>', ListaConversoesViewSet, name="conversor"),
     path('listagem', ListaConversoes.as_view(), name="listagem")
 ]
