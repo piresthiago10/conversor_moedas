9864ae0ed3f9ba963123e5a1f418847fbe7ebe97
piresthiago10 <thiagopires.as@gmail.com>
2021-09-27 00:00:35 -0300

    alteracao em atributos da classe

--- conversor_moedas/models.py
+++ conversor_moedas/models.py
@@ -11,8 +11,8 @@ class Conversao(models.Model):
    moeda_origem = models.CharField(max_length=3, choices=MOEDA, blank=False, default='USD')
    moeda_final = models.CharField(max_length=3, choices=MOEDA, blank=False, default='BRL')
    valor_conversao = models.FloatField()
-   valor_convetido = models.FloatField()
-   data_hora = models.DateField()
+   valor_convertido = models.FloatField(null=True)
+   data_hora = models.DateField(auto_now=True)
 
    def __str__(self) -> str:
        return self.moeda_origem + "-" + self.moeda_final
